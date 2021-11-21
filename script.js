9ba764446812427ce89894bfda2c1f196d0f1e25
Vienna Borowska <vienna.borowska@hotmail.com>
2021-11-21 19:02:15 +0000

    added times for timeblocks - LH Col

--- script.js
+++ script.js
@@ -1,33 +1,110 @@
 //Display current time and date in header and keep time with SetInterval 
-function displayTime() {
-    var rightNow = moment().format('MMM DD, YYYY [at] hh:mm:ss a');
+function displayTopTime() {
+    var rightNow = moment().format('dddd MMMM DD YYYY [at] hh:mm:ss a');
     $('#currentDay').text(rightNow);
   }
 
-setInterval(displayTime, 1000);
-displayTime();
+setInterval(displayTopTime, 1000);
+displayTopTime();
 
 //Create timeblocks for planner
-var timeBox = $('#container');
+var workHours = [
+    {
+        id: "0",
+        time: "09",
+        hour: "09",
+        ampm: "am",
+        savedNotes: ""
+        
+    },
 
-var times = [
-    '09AM',
-    '10AM',
-    '11AM',
-    '12PM',
-    '1PM',
-    '2PM',
-    '3PM',
-    '4PM',
-    '5PM',
-]; 
+    {
+        id: "1",
+        time: "10",
+        hour: "10",
+        ampm: "am",
+        savedNotes: ""
+        
+    },
 
+    {
+        id: "2",
+        time: "11",
+        hour: "11",
+        ampm: "am",
+        savedNotes: ""
+        
+    },
 
-for (var i = 0; i < times.length; i++) {
-    var timeBlocks = $('<input>');
-    var timeLabel = $('<label>');
-    timeLabel.text(times[i]);
-    timeBlocks.addClass('time-block'); 
-    timeBox.append(timeBlocks);
-}
+    {
+        id: "3",
+        time: "12",
+        hour: "12",
+        ampm: "pm",
+        savedNotes: ""
+        
+    },
 
+    {
+        id: "4",
+        time: "13",
+        hour: "01",
+        ampm: "pm",
+        savedNotes: ""
+        
+    },
+
+    {
+        id: "5",
+        time: "14",
+        hour: "02",
+        ampm: "pm",
+        savedNotes: ""
+        
+    },
+
+    {
+        id: "6",
+        time: "15",
+        hour: "03",
+        ampm: "pm",
+        savedNotes: ""
+        
+    },
+
+    {
+        id: "7",
+        time: "16",
+        hour: "04",
+        ampm: "pm",
+        savedNotes: ""
+        
+    },
+
+    {
+        id: "8",
+        time: "17",
+        hour: "05",
+        ampm: "pm",
+        savedNotes: ""
+        
+    },
+]
+
+//Display timeblocks
+
+workHours.forEach(function(eachHour) {
+    var hourBlock = $("<form>").attr({"class" : "row"});
+        $(".container").append(hourBlock);
+
+    var blockSpace = $("<div>").text(eachHour.hour + eachHour.ampm).attr({"class" : "col-md-2 hour"});
+
+    hourBlock.append(blockSpace);
+
+    var hourNotes = $("<div>").attr({"class" : "col-md-9 description p-0"});
+
+    var hourData = $("<textarea>");
+        hourNotes.append(hourData);
+        hourData.attr("id", eachHour.id)
+});
+    
